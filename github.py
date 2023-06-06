78cd477567a8ec0293ff8e52d866e1c20fe6b660
= <edy.prasetyo@indorent.co.id>
2023-06-06 17:50:57 +0700

    update

--- github.py
+++ github.py
@@ -4,33 +4,42 @@ import os
 import requests
 
 username = "edyprasetyo"
-access_token = "your_access_token"
+access_token = "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"
 
 clone_path = "/home/edy/Documents/MyProject/"
 clone_url = f"https://{username}:{access_token}@github.com/{username}"
 
-def get_github_repos(username):
-    """Gets all the GitHub repositories for a given user."""
-    url = "https://api.github.com/users/{}/repos".format(username)
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception("Could not get GitHub repositories for user: {}".format(username))
+headers = {
+    'Authorization': f'Token {access_token}'
+}
 
-
-repositories = get_github_repos(username)
-
-
-for repo in repositories:
-    print(repo["name"])
-    repo_path = os.path.join(clone_path, repo["name"])
-    clone_repo_url = f"{clone_url}/{repo['name']}.git"
-    git_command = ""
-    if os.path.isdir(repo_path):
-        git_command = f"cd {repo_path} && git pull"
-    else:
-        git_command = f"git clone {clone_repo_url} {repo_path}"
+def cloneRepo(repositories):
     
-    print(git_command)
-    os.system(git_command)
+    for repo in repositories:     
+        repo_path = os.path.join(clone_path, repo["name"])
+        clone_repo_url = f"{clone_url}/{repo['name']}.git"
+        git_command = ""
+        if os.path.isdir(repo_path):
+            git_command = f"cd {repo_path} && git pull"
+        else:
+            git_command = f"git clone {clone_repo_url} {repo_path}"
+        
+        print(git_command)
+        os.system(git_command)
+
+
+
+# Get public repositories
+public_repo_url = f'https://api.github.com/users/{username}/repos'
+response = requests.get(public_repo_url, headers=headers)
+public_repos = response.json()
+
+# Get private repositories
+private_repo_url = f'https://api.github.com/user/repos'
+response = requests.get(private_repo_url, headers=headers)
+private_repos = response.json()
+
+cloneRepo(public_repos)
+cloneRepo(private_repos)
+
+
